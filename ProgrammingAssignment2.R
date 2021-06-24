8fd58a659c2d4cf372291f81cfda56957bc8446a
3ahad <77240414+3ahad@users.noreply.github.com>
2021-06-24 13:56:32 +0300

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -2,56 +2,35 @@
 makeCacheMatrix  <- function(x = matrix()) {
         
         i <- NULL # Initialise the inverse property
-        
-        set <- function(y){
+        set <- function(y){ # set the matrix
                 x <<- y
                 i <<- NULL
-        } # Method to set the matrix
-        
-        get <- function(){
+        }   
+        get <- function(){ # get the matrix
                 x
-        } # Method to get the matrix
-
-        setInverse <- function(inverse){
+        } 
+        setInverse <- function(inverse){ #set the inverse of the matrix
                 i <<- inverse
-        } # Method to set the inverse of the matrix
-        
+        } 
         getInverse <- function() i # Return a list of the methods
         
         list(set = set, get = get,
              setInverse = setInverse,
              getInverse = getInverse)
-        
 }
 
 cacheSolve <- function(x, ...) {
         
-        m <- x$getInverse() ## Return a matrix that is the inverse of 'x'
-        
-        
+        m <- x$getInverse() #Return a matrix that is the inverse of 'x'
+        #OR return the inverse if it is already set
         if(!is.null(m)){
                 message("getting cached data")
                 return(m)
-        } # Return the inverse if it is already set
-        
-        
-        data <- x$get()  # Get the matrix from our object
-        
-        
+        } 
+        data <- x$get()  # Get the matrix from our object   
         m <- solve(data,... ) # Calculate the inverse matrix 
-        
         x$setInverse(m) # Set inverse to the object
-        
-        m  #Return the matrix
-        
+  
+        m  #Return the matrix   
 }
 
-
-
-x <- makeCacheMatrix(matrix(rnorm(9) , 3 , 3)) 
-#create a matrix as an argument to the new function
-
-
-cacheSolve(x) #then pass the result of a makeCacheMatrix call to cacheSolve
-
-cacheSolve(x) #it will return the cached value
